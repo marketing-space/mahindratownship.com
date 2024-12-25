873b1053236b0b041968a95ccb27ea6a40fd7fdb
thevinodpatidar <pvinod813@gmail.com>
2024-12-25 20:36:18 +0530

    Enhance Garden City layout with dynamic metadata configuration
    - Added site-specific metadata for SEO, including title and description, utilizing the new site configuration.
    - Integrated the getSiteConfig function to fetch relevant site details for improved content management.
    - Updated layout structure to support dynamic rendering of metadata, enhancing overall site performance and SEO optimization.

--- app/(sites)/Gardencity/layout.tsx
+++ app/(sites)/Gardencity/layout.tsx
@@ -1,4 +1,15 @@
+import { getSiteConfig } from "@/lib/config";
 import "./globals.css";
+
+import { Metadata } from "next";
+import { routes } from "@/lib/config/routes";
+
+const route = routes.Gardencity;
+
+export const metadata: Metadata = {
+  title: getSiteConfig(route).seo.title,
+  description: getSiteConfig(route).seo.description,
+};
 
 export default function GardenCityLayout({
   children,
