39dcc84d9ab446bc7307aa49b46969828ecd2ac5
marketing-space <admin@marketingspace.in>
2025-01-19 18:16:11 +0530

    add new page and route for Mahalaxmi

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,10 +1,12 @@
 import { gardenCityConfig } from "@/lib/config/garden-city";
+import { mahalaxmiConfig } from "@/lib/config/mahalaxmi";
 import { routes } from "./routes";
 import { siteConfig } from "./site";
 
 const configs = {
   [routes.default]: siteConfig,
   [routes.Gardencity]: gardenCityConfig,
+  [routes.Mahalaxmi]: mahalaxmiConfig,
 };
 
 export function getSiteConfig(site?: string) {
--- lib/config/routes.ts
+++ lib/config/routes.ts
@@ -1,4 +1,5 @@
 export const routes = {
   default: "/",
   Gardencity: "Gardencity",
+  Mahalaxmi: "Mahalaxmi",
 };
